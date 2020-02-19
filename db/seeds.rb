2555fb50f4eaa1af39bf2d0a3936ed445e429b16
Isabel Lopez <ilopezg31@gmail.com>
2020-02-19 14:51:55 +0100

    yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create(
+  name: 'Don Lee',
+  address: 'London St. 2020',
+  category: 'chinese'
+)
+
+Restaurant.create(
+  name: 'Mamma Mia',
+  address: 'Madrid 101',
+  category: 'italian'
+)
+
+Restaurant.create(
+  name: 'Sushi sushi',
+  address: 'Serrano St 001',
+  category: 'chinese'
+)
+
+Restaurant.create(
+  name: 'Pizzzza',
+  address: 'Juan de Mariana 15',
+  category: 'italian'
+)
+
+Restaurant.create(
+  name: 'French Fries',
+  address: 'Atocha st',
+  category: 'french'
+)
